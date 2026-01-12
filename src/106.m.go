d4d1f4da7a4a0ec6e3e08b45d7b0397db48d12bd
lucas <lucaswoo583@gmail.com>
2026-01-11 21:31:26 -0700

    106

--- src/106.m.go
+++ src/106.m.go
@@ -8,5 +8,33 @@ type TreeNode struct {
 }
 
 func buildTree(inorder []int, postorder []int) *TreeNode {
+  pointer := len(postorder) - 1;
+  return mergeTree(inorder, postorder, &pointer)
+}
+
+func mergeTree(inorder []int, postorder []int, pointer *int) *TreeNode {
+  if len(inorder) == 0 {
     return nil
+  }
+  cur := &TreeNode{
+    Val: postorder[(*pointer)],
+    Left: nil,
+    Right: nil,
+  }
+  curIndex := findIndex(postorder[(*pointer)], inorder)
+  (*pointer)--;
+  if curIndex != len(inorder) {
+    cur.Right = mergeTree(inorder[curIndex+1:], postorder, pointer)
+  }
+  cur.Left = mergeTree(inorder[:curIndex], postorder, pointer)
+  return cur
+}
+
+func findIndex(n int, inorder []int) int {
+  for i, v := range inorder {
+    if v == n {
+      return i
+    }
+  }
+  return 0;
 }
