661f0144ef84cf844db08c0f11209afc27b811ee
Lucas <lucaswoo583@gmail.com>
2026-01-18 18:52:12 -0700

    1.e.go

---
+++ src/1.e.go
@@ -0,0 +1,25 @@
+package main
+//redeclared
+func twoSummmmmmmm(nums []int, target int) []int {
+    var hashmap map[int]int = make(map[int]int);
+		var mySlice [] int = []int{};
+    var i int = 0;
+    for i < len(nums) {
+
+			var temp int = target - nums[i]
+			v, exists := hashmap[temp]
+			if exists {
+				mySlice = append(mySlice, v, i);
+				break;
+			}
+
+			_, ok := hashmap[nums[i]]
+      if ok {
+				i++;
+				continue;
+			}			
+			hashmap[nums[i]] = i;
+      i++;
+    }
+	return mySlice
+}
