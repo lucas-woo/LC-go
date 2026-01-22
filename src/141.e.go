6b1f1b9dccee3bdc358222966d159522602e11a1
lucas <lucaswoo583@gmail.com>
2026-01-22 14:08:51 -0700

    141

---
+++ src/141.e.go
@@ -0,0 +1,24 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func hasCycle(head *ListNode) bool {
+    slow := head;
+    fast := head;
+    for fast != nil && fast.Next != nil && fast.Next.Next != nil {
+        fast = fast.Next.Next;
+        if fast == slow {
+            return true
+        }
+        slow = slow.Next
+        if fast == slow {
+            return true
+        }         
+    }
+    return false
+}
