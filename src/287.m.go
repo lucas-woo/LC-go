ccd97bf55772ff830905934ff382413884270d57
lucas <lucaswoo583@gmail.com>
2026-02-07 13:23:20 -0700

    287

---
+++ src/287.m.go
@@ -0,0 +1,19 @@
+package main
+
+func findDuplicate(nums []int) int {
+	slow := 0;
+	fast := 0
+	for {
+		slow = nums[slow]
+		fast = nums[nums[fast]]
+		if slow == fast {
+			break
+		}
+	}
+	slow = nums[0]
+	for slow != fast {
+		slow = nums[slow]
+		fast = nums[fast]
+	}
+	return nums[slow]
+}
