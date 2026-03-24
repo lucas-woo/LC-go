a77bc6c25f251913e424fe9489b93ee5fb92d9c0
lucas <lucaswoo583@gmail.com>
2026-03-23 19:01:36 -0600

    2906

---
+++ src/2906.go
@@ -0,0 +1,33 @@
+package main
+
+func constructProductMatrix(grid [][]int) [][]int {
+	n := len(grid)
+	m := len(grid[0])
+	left := make([]int, n*m)
+	right := make([]int, n*m)
+	flat := make([]int, n*m)
+	ind := 0
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			flat[ind] = grid[i][j]
+			ind++
+		}
+	}
+	prod := 1
+	for i := 0; i < len(flat); i++ {
+		left[i] = prod
+		prod = (prod * flat[i]) % 12345
+	}
+	prod = 1
+	for i := len(flat) - 1; i >= 0; i-- {
+		right[i] = prod;
+		prod = (prod * flat[i]) % 12345
+	}
+	fmt.Println(left, right)
+	for i := 0; i < len(grid); i++ {
+		for j := 0; j < len(grid[i]); j++ {
+			grid[i][j] = (left[(i * m) + (j)] * right[(i * m) + (j)]) % 12345
+		}
+	}
+	return grid
+}
